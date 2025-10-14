82c852d8b50fc3f140c08da5e82f13dbb47ac59f
NMGpl <newmastergamingpl+github@gmail.com>
2025-10-14 23:43:56 +0200

    -add moving worm, -add working segments

--- Projekt/Projekt.cpp
+++ Projekt/Projekt.cpp
@@ -7,7 +7,7 @@ public:
     int x, y;
     vector<vector<int>> board;
     void initializeBoard() {
-        board = vector<vector<int>>(x, vector<int>(y, 0));
+        board = vector<vector<int>>(x, vector<int>(y, 1));
     }
     void printBoard() {
         cout << "Hodowla robakow o wielkosci " << x << "x" << y << "\n";
@@ -63,6 +63,10 @@ public:
         }
         
     }
+    // Setter
+    void setWorm(int x, int y) {
+        board[x][y] = 8;
+    }
     int getX() {
         return this->x;
     }
@@ -76,10 +80,16 @@ public:
     int age = 0;
     int m = 0;
     int coords[2] = { 4, 4 };
-    
 
-    float hunger;
-    void move(Board &board) {
+    int hunger = 1000;
+    int bodySegments = 5;
+    vector<vector<int>> bodyCoords;
+
+    Worm() {
+        bodyCoords.push_back({ coords[0], coords[1] });
+    }
+
+    void moveHead(Board &board) {
         int dx[4] = { 0, -1, 0, 1 };
         int dy[4] = { 1, 0, -1, 0 };
 
@@ -87,19 +97,70 @@ public:
         
         int newX = coords[0] + dx[direction];
         int newY = coords[1] + dy[direction];
-
-        if (newX >= 0 && newX < board.getX() && newY >= 0 && newY < board.getY()) {
-            coords[0] = newX;
-            coords[1] = newY;
+        if (hunger > 0) {
+            hunger -= 1;
+            cout << "Moj glod to: " << hunger << " / 25\n";
+            if (newX >= 0 && newX < board.getX() && newY >= 0 && newY < board.getY() && isBody(newX, newY)) {
+                //moveBody(coords);
+                moveBody(board);
+                coords[0] = newX;
+                coords[1] = newY;
+            }
+            drawWorm(board);
+            cout << "Jestem w x: " << coords[0] << " y: " << coords[1] << "\n";
+        }else {
+            cout << "Zmarlem z glodu :( \n";
         }
-        cout << "Jestem w x: " << coords[0] << " y: " << coords[1] << "\n";
+    }
+    //void moveBody(int headCoords[2]) {
+    //    vector<vector<int>> bodyCoords;
+    //    bodyCoords = vector<vector<int>>(bodySegments, vector<int>(2));
+    //    for (int i = bodySegments - 1; i >= 0; i--) {
+    //        if (i > 0) {
+    //            bodyCoords[i][0] = bodyCoords[i - 1][0];
+    //            bodyCoords[i][1] = bodyCoords[i - 1][1];
+    //        }else{
+    //            bodyCoords[0][0] = headCoords[0];   //x pierwszego segmentu = x glowy
+    //            bodyCoords[0][1] = headCoords[1];   //y pierwszego segmentu = y glowy
+    //        }
+    //    }
+    //    cout << "Koordynaty glowy - x: " << coords[0] << " y: " << coords[1] << endl;
+    //    cout << "Koordynaty 1. segmentu - x: " << bodyCoords[0][0] << " y: " << bodyCoords[0][1] << endl;
+    //}
+    void moveBody(Board &board) {
+        if (bodyCoords.size() < bodySegments) {
+            vector<int> last = bodyCoords.back();
+            while (bodyCoords.size() < bodySegments) {
+                bodyCoords.push_back(last);
+            }
         }
+        for (int i = bodySegments - 1; i > 0; i--) {
+            bodyCoords[i] = bodyCoords[i - 1];
+        }
+        bodyCoords[0] = { coords[0], coords[1] }; // głowa sprzed ruchu
+        cout << "Koordynaty 1. segmentu - x: " << bodyCoords[0][0] << " y: " << bodyCoords[0][1] << endl;
+    }
     void ageUp() {
         if (age < maxAge) {
             age += 1;
             cout << "Mam " << age << " lat, maksymalnie przezyje " << maxAge;
         }if (age > 3) {
             //rozmnazanie
+        }
+    }
+    bool isBody(int newX, int newY) {
+        for (size_t i = 0; i < bodyCoords.size(); i++) {
+            if (newX == bodyCoords[i][0] && newY == bodyCoords[i][1]) {
+                cout << "Nie moge tu wejsc \n";
+                return false;
+            }
+        }
+        return true;
+    }
+    void drawWorm(Board &board) {
+        board.setWorm(coords[0], coords[1]);
+        for (int i = bodySegments - 1; i >= 0; i--) {
+            board.setWorm(bodyCoords[i][0], bodyCoords[i][1]);
         }
     }
 };
@@ -109,24 +170,23 @@ int main(){
     int days = 0;
     Board plansza;
     Worm worm;
-    plansza.x = 10;
-    plansza.y = 10;
+    plansza.x = 20;
+    plansza.y = 40;
     plansza.initializeBoard();
-    plansza.printBoard();
+    //plansza.printBoard();
     //plansza.startWormColony();
     while (true) {
         //plansza.printBoard();
         //plansza.wormDistribution();
         cout << "Uplynelo: " << days << " tur\n";
         days += 1;
+        worm.moveHead(plansza);
         plansza.printBoard();
-        worm.move(plansza);
+        plansza.initializeBoard();
         worm.ageUp();
-        cin >> stop;
-        if (stop == 111) {
-            break;
-        }
         cout << endl;
+        system("pause");
+        system("cls");
     }
     return 0;
 }
